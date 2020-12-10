688ef7958cec2d10a3f14942863f2148dc3bc8a1
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-12-10 13:42:12 +0100

    init: chart component

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
-// eslint-disable-next-line import/extensions
+/* eslint-disable import/extensions */
 import MapComponent from './map.component.js';
+import ChartComponent from './chart.component.js'
 
 export default class App {
   constructor() {
     this.mapComponent = new MapComponent(/*data*/);
+    this.chartComponent = new ChartComponent();
   }
 
   init() {
     const appContainer = document.querySelector('.app-container');
     appContainer.append(this.mapComponent.render());
+    appContainer.append(this.chartComponent.render());
+
     return appContainer;
   }
 }
